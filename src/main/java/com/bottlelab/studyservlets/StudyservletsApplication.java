a085af2cec731d45c286a9a83700e3c7dcb1f0b0
byeongjuPark <bottlepark28@gmail.com>
2022-12-13 14:55:34 +0900

    first commit

---
+++ src/main/java/com/bottlelab/studyservlets/StudyservletsApplication.java
@@ -0,0 +1,13 @@
+package com.bottlelab.studyservlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyservletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyservletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bottlelab/studyservlets/StudyservletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottlelab.studyservlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyservletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
